8cf0e796c07c6fa6d2f2741c1739cf228f234333
Kiedae <ernestwilliams335@gmail.com>
2023-08-16 16:13:52 -0400

    added function to add/remove score and time based on answer

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var startquiz = document.querySelector("#startbutton");
 var quizTimer = document.getElementById("timeleft");
+var playerscore = document.getElementById("score");
 var secondsLeft = 45;
 var score = 0;
 // holds the questions thier choices and correct answer
@@ -7,27 +8,27 @@ var questions = [
 
  { question: "What language can we use to create a simple webpage?",
    choices: ["HTML", "CSS", "Javascript", "All of the above"],
-   correctAnswerIndex: 4
+   correctAnswerIndex: 3
 
  },
 
  {
   question: "What is the correct design of a simple for loop?",
   choices: ["<script>", "<javascript>", "<script.js>", "<insert>"],
-  correctAnswerIndex: 1
+  correctAnswerIndex: 0
 
  },
 
  {
   question: "What is a boolean?",
   choices: ["A foreign food", "a true or false statement", "a string of numbers", "an array of numbers"],
-  correctAnswerIndex: 2
+  correctAnswerIndex: 1
  },
 
  {
   question: "What does the === operator in Javascript do?",
   choices: ["Checks for Strict equality of values", "Assigns a value to an object", "Sets a value to a variable", "Asks to take the object out on a date"],
-  correctAnswerIndex: 1
+  correctAnswerIndex: 0
 
  }
 
@@ -38,9 +39,11 @@ startquiz.addEventListener("click", function() {
    //hides the startbutton once the quiz is started
   startquiz.style.display = "none";
 
+  // starts the timer for the quiz
 timerClock();
 // Starts the quiz, displays the first question within questions variable
 displayQuestion();
+
 });
 
 function timerClock() {
@@ -49,7 +52,7 @@ function timerClock() {
       secondsLeft--;
       //Shows user how much time is left on quiz
     quizTimer.textContent = secondsLeft + " seconds left until your quiz is over.";
-
+playerscore.textContent = "Score: " + score;
     
 //when the secondsLeft variable is 0 stop the function
      if(secondsLeft == 0) {
@@ -85,5 +88,26 @@ function displayQuestion() {
   });
 }
 
+function checkAnswer(selectedIndex) {
+  var correctIndex = questions[currentQuestionIndex].correctAnswerIndex;
+
+  if (selectedIndex === correctIndex) {
+ // adds score and time if correct answer
+    score+=5;
+    secondsLeft+=5;
+  } else {
+//subtracts score and time if wrong answer
+    score-=5;
+secondsLeft-=5;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questions.length) {
+    displayQuestion(); // Move to the next question
+  } else {
+    // will eventually end quiz
+  }
+}
 
 
